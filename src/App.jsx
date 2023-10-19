a48590cc55fdf4b598774790822f1044d4e85341
Abhishek280999 <darwekar155@gmail.com>
2023-10-19 12:11:21 +0530

    Route

--- src/App.jsx
+++ src/App.jsx
@@ -16,7 +16,7 @@ const App = () => {
           <Route path='/sign-in' element={<SignIn />} />
           <Route path='/sign-up' element={<SignUp />} />
           <Route path='/about' element={<About />} />
-          <Route path='/profile' element={<Profile />} />
+          <Route path='/profile' element={<Profile />}  />
 
         </Routes>
       </BrowserRouter>
