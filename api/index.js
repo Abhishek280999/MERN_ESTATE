2af6d6cfb391a09f2d6753463c1f7d9fe5e98732
Abhishek280999 <darwekar155@gmail.com>
2023-10-20 11:43:23 +0530

    test api route

--- api/index.js
+++ api/index.js
@@ -1,6 +1,7 @@
 import express from 'express'
 import mongoose from 'mongoose'
 import dotenv from 'dotenv'
+import userRouter from './routes/user.routes.js'
 
 dotenv.config()
 
@@ -17,3 +18,9 @@ const app = express();
 app.listen(3000, () => {
     console.log("Server is srnning on port 3000  ")
 })
+
+app.use('/api/user',userRouter)
+
+// app.get('/test', ( req , res) =>{
+//    res.send("hello world") 
+// } )
---
+++ api/routes/user.routes.js
@@ -0,0 +1,13 @@
+import express from 'express'
+
+
+
+const router = express.Router();
+
+router.get('/test', ( req , res) =>{
+    res.json({
+        message : 'Api route Working'
+    }) 
+ } )
+
+ export default router
